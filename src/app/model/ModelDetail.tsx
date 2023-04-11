cfc6b8749e6b56847413b9336a21163065732435
Ramandeep <ramank6238299@gmail.com>
2023-04-11 19:02:41 +0530

    changed ui

--- src/app/model/ModelDetail.tsx
+++ src/app/model/ModelDetail.tsx
@@ -104,9 +104,7 @@ export const ModelDetailPage = () => {
           <div className="grid items-stretch grid-cols-1 gap-y-12 lg:grid-cols-12 gap-x-16">
             <div className="flex flex-col justify-between lg:col-span-8">
               <div className="max-w-md">
-                <h2 className="text-3xl font-bold text-gray-900">
-                  Our Roadmap
-                </h2>
+                <h2 className="text-3xl font-bold text-gray-900">Model Name</h2>
                 <p className="mt-4 text-base font-medium text-gray-500">
                   In a creative workplace, employees with responsibly try
                   different solutions
@@ -121,31 +119,157 @@ export const ModelDetailPage = () => {
                             href="#"
                             className="py-4 text-sm font-medium text-indigo-600 transition-all duration-200 border-b-2 border-indigo-600 whitespace-nowrap"
                           >
-                            {" "}
-                            Dashboard{" "}
+                            Details
                           </a>
 
                           <a
                             href="#"
                             className="py-4 text-sm font-medium text-gray-900 transition-all duration-200 border-b-2 border-transparent hover:border-gray-300 whitespace-nowrap"
                           >
-                            {" "}
-                            My Account{" "}
+                            Demo
                           </a>
 
                           <a
                             href="#"
                             className="py-4 text-sm font-medium text-gray-900 transition-all duration-200 border-b-2 border-transparent hover:border-gray-300 whitespace-nowrap"
                           >
-                            {" "}
-                            Downloads{" "}
+                            API
                           </a>
                         </nav>
                       </div>
                     </div>
                   </div>
                 </div>
+
+                <div className="pb-[1rem] bg-white">
+                  <div className=" mx-auto  max-w-7xl">
+                    <div className="max-w-xl mx-auto">
+                      <div>
+                        <label className="block text-sm font-bold text-gray-900">
+                          Prompt
+                        </label>
+                        <div className="mt-2">
+                          <div className="mt-2">
+                            <textarea
+                              name=""
+                              id=""
+                              placeholder="Enter Prompt"
+                              rows="1"
+                              className="border block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-lg resize-y focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600"
+                            ></textarea>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* <div className="flex gap-[10px] mt-[4px]">
+                        <button className="btn btn-outline-active">20</button>
+                        <button className="btn btn-outline">30</button>
+                        <button className="btn btn-outline">50</button>
+                      </div> */}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="pb-[1rem] bg-white">
+                  <div className=" mx-auto  max-w-7xl">
+                    <div className="max-w-xl mx-auto">
+                      <div>
+                        <label className="block text-sm font-bold text-gray-900">
+                          Steps
+                        </label>
+                        <div className="mt-2">
+                          <div className="mt-2">
+                            <div className="flex gap-[10px] mt-[4px]">
+                              <button className="btn border bg-[#000] text-[#fff] border-black text-[#000]">
+                                30
+                              </button>
+                              <button className="btn border border-black text-[#000]">
+                                30
+                              </button>
+                              <button className="btn border border-black text-[#000]">
+                                50
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
+
+              {/* <div className="mt-2">
+                        <div className="mt-2">
+                          <div className="py-8 bg-white">
+                            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                              <div className="max-w-xs mx-auto">
+                                <div className="absolute top-0 left-0 w-full h-screen z-0 bg-transparent"></div>
+                                <div className="relative">
+                                  <label className="block text-sm font-bold text-gray-900">
+                                    
+                                    Country
+                                  </label>
+                                  <div className="mt-2">
+                                    <div className="cursor-pointer block w-full py-3 px-4 border-gray-300 border rounded-lg focus:outline-none focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm">
+                                      <div className="flex justify-between items-center">
+                                        <div className="flex items-center justify-start space-x-2">
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-4 w-4 text-gray-400"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            stroke-width="2"
+                                          >
+                                            <path
+                                              stroke-linecap="round"
+                                              stroke-linejoin="round"
+                                              d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                                            />
+                                          </svg>
+                                          <span>Select a country</span>
+                                        </div>
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          className="h-4 w-4"
+                                          fill="none"
+                                          viewBox="0 0 24 24"
+                                          stroke="currentColor"
+                                          stroke-width="2"
+                                        >
+                                          <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M19 9l-7 7-7-7"
+                                          />
+                                        </svg>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div
+                                    className="relative -bottom-2 w-full z-10"
+                                    x-show="open"
+                                  >
+                                    <div className="border-gray-300 bg-white shadow border rounded-lg w-full block text-sm px-4 py-2 space-y">                                     <ul className="flex flex-col">
+                                        <li className="w-full rounded-md p-2 hover:bg-gray-100 cursor-pointer">
+                                          🇺🇸 United States of America
+                                        </li>
+                                        <li className="w-full rounded-md p-2 hover:bg-gray-100 cursor-pointer">
+                                          🇳🇱 Netherlands
+                                        </li>
+                                        <li className="w-full rounded-md bg-gray-100 p-2 hover:bg-gray-100 cursor-pointer">
+                                          🇳🇿 New Zealand
+                                        </li>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                     */}
 
               {/* <div className="grid grid-cols-1 gap-12 mt-12 sm:grid-cols-2 sm:mt-16 xl:gap-x-16">
                 <div>
